02cc7e06e2f80a27484cabb1cec1dc405731b0ba
Saurav <ghimiresaurav789@gmail.com>
2026-03-17 11:44:31 +0545

    chore: coonection to database

---
+++ src/core/database.js
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+import  logger from "../utils/logger.js";
+
+dotenv.config();
+
+class Database {
+    constructor() {
+        if (!Database.instance) {
+            this.sequelize = new Sequelize (
+                process.env.DB_NAME,
+                process.env.DB_USER,
+                process.env.DB_PASSWORD,
+                {
+                    host: process.env.DB_HOST,
+                    dialect: process.env.DB_DIALECT,
+                    logging: (msg) => logger.debugContext(msg),
+
+                    pool: {
+                        max: 5,
+                        min: 0,
+                        acquire: 30000,
+                        idle: 10000
+                    }
+                }
+            );
+            Database.instance = this;
+        }
+        return Database.instance;
+    }
+    async connect () {
+        try {
+            await this.sequelize.authenticate();
+            logger.info(' connect');
+        } catch (error) {
+            logger.error('Unable to connect', error);
+            process.exit(1);
+        }
+    }
+
+}
+
+const instance = new Database();
+export default instance;
+
